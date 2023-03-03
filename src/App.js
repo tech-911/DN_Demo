79c31ff2adb98ed1514394d1fe612fc4b3533902
tech-911 <babatundeolatunji74@gmail.com>
2023-03-03 12:01:37 +0100

    ...first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita
+        perspiciatis consequatur quas, cum maxime nisi neque nobis odio
+        assumenda inventore nihil alias provident et saepe! Molestiae
+        consequatur quibusdam rem quisquam?
+      </div>
+    </div>
+  );
+};
+
+export default App;
