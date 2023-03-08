ab5a76fbfaadfd9cf33678eb9c1fef51370102f4
tech-911 <babatundeolatunji74@gmail.com>
2023-03-08 23:39:15 +0100

    working on lecturers

--- src/pages/lecturers/Lecturers.jsx
+++ src/pages/lecturers/Lecturers.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./lecturers.scss";
 import Container from "../../components/container/Container";
 import FilterButton from "../../components/filterButton/FilterButton";
+import { lecturers, language, alphabet } from "./data";
 
 const Lecturers = () => {
   const [filter, setFilter] = useState();
@@ -10,14 +11,21 @@ const Lecturers = () => {
     <Container>
       <div className="lecturers_wrapper">
         <div className="lecturers_filter">
-          <FilterButton
-            filter={filter}
-            setFilter={setFilter}
-            active={active}
-            setActive={setActive}
-            title="All"
-            action="all"
-          />
+          <div className="lecturers_filter_name">
+            {lecturers.map(({ lecturer, id }, idx) => {
+              <FilterButton
+                filter={filter}
+                setFilter={setFilter}
+                active={active}
+                setActive={setActive}
+                title="All"
+                action=""
+              />;
+            })}
+          </div>
+          <div className="lecturers_filter_language"></div>
+          <div className="lecturers_filter_alphabet"></div>
+
           <FilterButton
             filter={filter}
             setFilter={setFilter}
---
+++ src/pages/lecturers/data.jsx
@@ -0,0 +1,76 @@
+export const lecturers = [
+  {
+    id: 0,
+    lecturer: "All",
+  },
+  {
+    id: 1,
+    lecturer: "Ustadh Abu Rahimah...",
+  },
+  {
+    id: 2,
+    lecturer: "Dr Nazifi Yunus Al-Bayaan",
+  },
+  {
+    id: 3,
+    lecturer: "Mallam Mustapha Biu",
+  },
+  {
+    id: 4,
+    lecturer: "Ustadh Qamarudee...",
+  },
+  {
+    id: 5,
+    lecturer: "Dr Sharaafudeen Raji",
+  },
+];
+export const language = [
+  {
+    id: 0,
+    language: "Yoruba",
+  },
+  {
+    id: 1,
+    language: "Hausa",
+  },
+  {
+    id: 2,
+    language: "English",
+  },
+  {
+    id: 3,
+    language: "Igala",
+  },
+  {
+    id: 4,
+    language: "Arabia",
+  },
+];
+export const alphabet = [
+  { alphabet: "a" },
+  { alphabet: "b" },
+  { alphabet: "c" },
+  { alphabet: "d" },
+  { alphabet: "e" },
+  { alphabet: "f" },
+  { alphabet: "g" },
+  { alphabet: "h" },
+  { alphabet: "i" },
+  { alphabet: "j" },
+  { alphabet: "k" },
+  { alphabet: "l" },
+  { alphabet: "m" },
+  { alphabet: "n" },
+  { alphabet: "o" },
+  { alphabet: "p" },
+  { alphabet: "q" },
+  { alphabet: "r" },
+  { alphabet: "s" },
+  { alphabet: "t" },
+  { alphabet: "u" },
+  { alphabet: "v" },
+  { alphabet: "w" },
+  { alphabet: "x" },
+  { alphabet: "y" },
+  { alphabet: "z" },
+];
