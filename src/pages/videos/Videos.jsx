791f1f838878c812e39e190e5ac9b836895c34a5
tech-911 <babatundeolatunji74@gmail.com>
2023-03-09 17:34:43 +0100

    working on video

--- src/pages/videos/Videos.jsx
+++ src/pages/videos/Videos.jsx
@@ -1,11 +1,68 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./videos.scss";
 import Container from "../../components/container/Container";
+import { categories } from "./data";
+import axios from "axios";
+import FilterButton from "../../components/filterButton/FilterButton";
+import AlbumWidget from "../../components/albumWidget/AlbumWidget";
 
 const Videos = () => {
+  const [data, setData] = useState([]);
+  const [filter, setFilter] = useState([]);
+  const [data1, setData1] = useState([]);
+  const [data2, setData2] = useState([]);
+  const [data3, setData3] = useState([]);
+  const [active, setActive] = useState("All");
+  const [active1, setActive1] = useState("");
+  const [active2, setActive2] = useState("");
+
+  useEffect(() => {
+    setData3(data);
+  }, [data]);
+
+  useEffect(() => {
+    axios
+      .get("https://www.dawahbox.com/mongo/api/leclisting_page_api.php?page=2")
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
     <Container>
-      <div className="video_wrapper">Videos</div>
+      <div className="video_wrapper">
+        <div className="video_filter">
+          <div className="video_filter_categories">
+            {categories.map(({ categories, id }, idx) => {
+              return (
+                <FilterButton
+                  key={idx}
+                  filter={filter}
+                  setFilter={setFilter}
+                  data1={data1}
+                  setData1={setData1}
+                  data2={data2}
+                  setData2={setData2}
+                  data3={data3}
+                  setData3={setData3}
+                  active={active}
+                  setActive={setActive}
+                  title={categories}
+                  action="categories"
+                  data={data}
+                />
+              );
+            })}
+          </div>
+        </div>
+        <div className="video_widget">
+          {filter.map(({ img, cats }, idx) => {
+            return <AlbumWidget key={idx} categories={cats} img={img} />;
+          })}
+        </div>
+      </div>
     </Container>
   );
 };
---
+++ src/pages/videos/data.jsx
@@ -0,0 +1,93 @@
+export const categories = [
+  {
+    id: 0,
+    categories: "All",
+  },
+  {
+    id: 1,
+    categories: "Charity",
+  },
+  {
+    id: 2,
+    categories: "Men",
+  },
+  {
+    id: 3,
+    categories: "Character & Manners",
+  },
+  {
+    id: 4,
+    categories: "Knowledge",
+  },
+  {
+    id: 5,
+    categories: "Q&A",
+  },
+  {
+    id: 6,
+    categories: "Jumuah",
+  },
+  {
+    id: 7,
+    categories: "Ramadan",
+  },
+  {
+    id: 8,
+    categories: "Sacred Months",
+  },
+];
+export const language = [
+  {
+    id: 0,
+    language: "All",
+  },
+  {
+    id: 1,
+    language: "Yoruba",
+  },
+  {
+    id: 2,
+    language: "Hausa",
+  },
+  {
+    id: 3,
+    language: "English",
+  },
+  {
+    id: 4,
+    language: "Igala",
+  },
+  {
+    id: 5,
+    language: "Arabia",
+  },
+];
+export const alphabet = [
+  { alphabet: "Hot" },
+  { alphabet: "A" },
+  { alphabet: "B" },
+  { alphabet: "C" },
+  { alphabet: "D" },
+  { alphabet: "E" },
+  { alphabet: "F" },
+  { alphabet: "G" },
+  { alphabet: "H" },
+  { alphabet: "I" },
+  { alphabet: "J" },
+  { alphabet: "K" },
+  { alphabet: "L" },
+  { alphabet: "M" },
+  { alphabet: "N" },
+  { alphabet: "O" },
+  { alphabet: "P" },
+  { alphabet: "Q" },
+  { alphabet: "R" },
+  { alphabet: "S" },
+  { alphabet: "T" },
+  { alphabet: "U" },
+  { alphabet: "V" },
+  { alphabet: "W" },
+  { alphabet: "X" },
+  { alphabet: "Y" },
+  { alphabet: "Z" },
+];
